d59bc8b981e5f74237b709938ebb740bb051eee1
gui2310g <fontesguilherme57@gmail.com>
2025-03-02 23:31:21 -0300

    [add] created admin, user and auth page with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent
+  },
+  {
+    path: "admin",
+    component: AdminPageComponent
+  },
+  {
+    path: "user",
+    component: UserPageComponent
+  }
+];
