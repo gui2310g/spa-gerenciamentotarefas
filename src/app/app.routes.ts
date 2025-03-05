a962b21381e0e72b3797db96ea1aea495730563d
gui2310g <fontesguilherme57@gmail.com>
2025-03-05 15:11:54 -0300

    [add] created layout page and added navbar in user and admin page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,16 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { UserPageComponent } from './pages/user-page/user-page.component';
+import { LayoutComponent } from './pages/layout-page/layout.component';
 
 export const routes: Routes = [
+  { path: '', component: AuthComponent },
   {
     path: '',
-    component: AuthComponent
-  },
-  {
-    path: "admin",
-    component: AdminPageComponent
-  },
-  {
-    path: "user",
-    component: UserPageComponent
+    component: LayoutComponent,
+    children: [
+      { path: "admin", component: AdminPageComponent },
+      { path: "user", component: UserPageComponent }
+    ]
   }
 ];
