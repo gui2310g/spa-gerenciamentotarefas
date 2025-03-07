fd5f59cd7a5a8574f05bf002a32f21ea677c4bd0
gui2310g <fontesguilherme57@gmail.com>
2025-03-06 22:35:11 -0300

    [add] added admin page template, style and typescript prototype

--- src/app/pages/admin-page/admin-page.component.ts
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -1,12 +1,94 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss'
 })
 export class AdminPageComponent {
+  showModal: boolean = false;
 
+  OpenModal(): void {
+    this.showModal = true
+  }
+
+  CloseModal(): void {
+    this.showModal = false
+  }
+
+  Usuarios: any[] = [
+    {
+      id: 1,
+      nome: 'Juan',
+      email: 'juanmendes@gmail.com'
+    },
+    {
+      id: 2,
+      nome: 'Pedro',
+      email: 'pedrolopes@orkut.com'
+    },
+    {
+      id: 3,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 4,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 5,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 6,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 7,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 8,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 9,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 3,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 3,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 3,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 3,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    },
+    {
+      id: 3,
+      nome: 'Maria',
+      email: 'sandymaria@hotmail.com'
+    }
+  ]
 }
